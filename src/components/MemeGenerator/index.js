ae53726fb508a1952431e5603db6e18078554a75
prashanth <reddyprashanth512@gmail.com>
2024-01-16 06:42:12 +0000

    solution

---
+++ src/components/MemeGenerator/index.js
@@ -0,0 +1,144 @@
+// Style your components here
+
+// ./src/components/MemeGenerator/index.js
+import {Component} from 'react'
+import {
+  MemeContainer,
+  Heading,
+  InputLabel,
+  ImageInput,
+  SelectLabel,
+  Select,
+  GenreatorButton,
+  GeneratedMeme,
+} from './styledComponents'
+
+const fontSizesOptionsList = [
+  {
+    optionId: '8',
+    displayText: '8',
+  },
+  {
+    optionId: '12',
+    displayText: '12',
+  },
+  {
+    optionId: '16',
+    displayText: '16',
+  },
+  {
+    optionId: '20',
+    displayText: '20',
+  },
+  {
+    optionId: '24',
+    displayText: '24',
+  },
+  {
+    optionId: '28',
+    displayText: '28',
+  },
+  {
+    optionId: '32',
+    displayText: '32',
+  },
+]
+
+class MemeGenerator extends Component {
+  state = {
+    topText: '',
+    bottomText: '',
+    fontSize: 9,
+    generatedMeme: null,
+    image: '',
+  }
+
+  onTopText = event => {
+    this.setState({topText: event.target.value})
+  }
+
+  onBottomText = event => {
+    this.setState({bottomText: event.target.value})
+  }
+
+  onFontSize = event => {
+    this.setState({fontSize: event.target.value})
+  }
+
+  onGenreateMeam = () => {
+    const {topText, bottomText, image} = this.state
+    const generatedMeme = (
+      <GeneratedMeme image={image}>
+        <div>
+          {topText && <p>{topText}</p>}
+          {bottomText && <p>{bottomText}</p>}
+        </div>
+      </GeneratedMeme>
+    )
+    this.setState({generatedMeme})
+  }
+
+  onImage = () => {
+    this.setState({
+      image: 'https://assets.ccbp.in/frontend/react-js/nature-img.png',
+    })
+  }
+
+  render() {
+    const {topText, bottomText, fontSize, generatedMeme, image} = this.state
+    return (
+      <MemeContainer>
+        <Heading>Meme Generator</Heading>
+        <form>
+          <InputLabel>
+            image URL
+            <ImageInput
+              type="text"
+              value={image}
+              onChange={this.onImage}
+              placeholder="Enter the imageURL"
+            />
+          </InputLabel>
+
+          <InputLabel>
+            Top Text
+            <ImageInput
+              type="text"
+              value={topText}
+              onChange={this.onTopText}
+              placeholder="Enter the Top Text"
+            />
+          </InputLabel>
+
+          <InputLabel>
+            Bottom Text
+            <ImageInput
+              type="text"
+              value={bottomText}
+              onChange={this.onBottomText}
+              placeholder="Enter the Bottom Text"
+            />
+          </InputLabel>
+
+          <SelectLabel>
+            Font Size
+            <Select value={fontSize} onChange={this.onFontSize}>
+              {fontSizesOptionsList.map(item => (
+                <option key={item.optionId} value={item.optionId}>
+                  {item.displayText}
+                </option>
+              ))}
+            </Select>
+          </SelectLabel>
+
+          <GenreatorButton onClick={this.onGenreateMeam}>
+            Generate
+          </GenreatorButton>
+        </form>
+        {generatedMeme && <div data-testid="meme">{generatedMeme}</div>}
+      </MemeContainer>
+    )
+  }
+}
+
+export default MemeGenerator
---
+++ src/components/MemeGenerator/styledComponents.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components'
+
+export const MemeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  align-items: center;
+`
+export const Heading = styled.h1`
+  font-family: 'Roboto';
+  color: #35469c;
+`
+export const InputLabel = styled.label`
+  font-family: 'Roboto';
+  color: #7e858e;
+  margin-top: 10px;
+`
+export const ImageInput = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  margin-bottom: 10px;
+`
+export const SelectLabel = styled.label`
+  font-family: 'Roboto';
+  color: #7e858e;
+  margin-top: 10px;
+`
+export const Select = styled.select`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+`
+export const GenreatorButton = styled.button`
+  background-color: #35469c;
+  height: 32px;
+  width: 99px;
+  border-width: 0px;
+  color: white;
+  font-family: 'Roboto';
+  border-radius: 5px;
+`
+
+export const GeneratedMeme = styled.div`
+  margin-top: 20px;
+  background-image: ${props => `url(${props.image})`};
+  padding: 20px;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 100%;
+    border-radius: 5px;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    color: #333;
+    text-align: center;
+    margin-top: 10px;
+  }
+`
